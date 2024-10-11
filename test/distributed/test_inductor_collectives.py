7b372351149e0bee98ca613ba6cb78ef9294e553
Will Feng <yf225@cornell.edu>
2024-10-10 23:58:29 -0700

    [WIP] TorchRec Request.wait() support
    ghstack-source-id: c7385aabb177a6369fe73f818d9bb910bb1cc4f2
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/137763

--- test/distributed/test_inductor_collectives.py
+++ test/distributed/test_inductor_collectives.py
@@ -245,6 +245,39 @@ class TestCollectivesMultiProc(DynamoDistributedMultiProcTestCase):
 
     @unittest.skipIf(not HAS_GPU, "Inductor+gpu needs triton and recent GPU arch")
     @skip_if_lt_x_gpu(2)
+    def test_eager_async_allreduce_inductor_wait(self):
+        import torch.distributed as dist
+
+        def all_reduce_eager(x):
+            y = x * x
+            for _ in range(100):
+                req = dist.all_reduce(y, op=dist.ReduceOp.SUM, async_op=True)
+                # assert isinstance(req, torch.distributed.Work)
+            return req, y
+
+        def all_reduce_wait(inputs):  # potentially compiled
+            req = inputs[0]
+            y = inputs[1]
+            if torch.compiler.is_dynamo_compiling():
+                torch.ops.c10d_functional.wait_tensor(y)
+            else:
+                req.wait()
+            return y
+
+        with _dynamo_dist_per_rank_init(self.rank, self.world_size):
+            all_reduce_wait_compiled = torch.compile(
+                all_reduce_wait,
+                backend="inductor",
+                fullgraph=True,
+            )
+
+            inputs = torch.ones(12800, 12800, device="cuda") + self.rank
+            out_ref = all_reduce_wait(all_reduce_eager(inputs))
+            out_compiled = all_reduce_wait_compiled(all_reduce_eager(inputs))
+            self.assertEqual(out_ref, out_compiled)
+
+    @unittest.skipIf(not HAS_GPU, "Inductor+gpu needs triton and recent GPU arch")
+    @skip_if_lt_x_gpu(2)
     @patch.object(torch._inductor.config, "allow_buffer_reuse", True)
     def test_allreduce_input_buffer_reuse(self):
         def func(a, *, tag, ranks, group_size):
